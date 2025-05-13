552f52173e4f51718c3deaecb137cc781c2c3c62
gustavoniglia <gustavoniglia2@gmail.com>
2025-05-13 20:28:30 -0300

    Estruturando meus arquivos

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Importa módules app e BroserWindow do Electron
-const {app, BrowserWindow,} = require('electron'); 267 (gzipped: 209)
+const { app, BrowserWindow, } = require('electron');
 
 // Função para criar a janela principal
 function CreateWindowns() {
 
 
-// Criar uma nova assitancia do BrowserWindow
+    // Criar uma nova assitancia do BrowserWindow
     const window = new BrowserWindow({
         width: 800, //largura da Janela
         height: 600, //largura da Janela
@@ -14,11 +14,10 @@ function CreateWindowns() {
 
 
     // Carregar o arquivo HTML na janela
-    window.loadFile('index.html')
-
-    // Evento que acionado quando o Electron está pronto
-    app.whenReady().then( () => {
-    CreateWindowns(); //Chama a função de criar a janela
-    } )
-
+    window.loadFile("src/pages/index.html")
 }
+
+// Evento que acionado quando o Electron está pronto
+app.whenReady().then(() => {
+    CreateWindowns(); //Chama a função de criar a janela
+})
